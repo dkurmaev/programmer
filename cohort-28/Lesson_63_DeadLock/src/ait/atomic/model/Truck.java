dc43d0d6f0cb1eb6df830745fa4c39b50d629cc5
dkurmaev <danilkurmayev@gmail.com>
2023-08-16 09:39:59 +0200

    new

---
+++ cohort-28/Lesson_63_DeadLock/src/ait/atomic/model/Truck.java
@@ -0,0 +1,21 @@
+package ait.atomic.model;
+
+public class Truck implements Runnable {
+
+    int nRaces;
+    int capacity;
+    Elevator elevator;
+
+    public Truck(int nRaces, int capacity, Elevator elevator) {
+        this.nRaces = nRaces;
+        this.capacity = capacity;
+        this.elevator = elevator;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < nRaces; i++) {
+            elevator.add(capacity);
+        }
+    }
+}
