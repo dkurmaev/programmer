de82478481b7938a6cbd999302a557818f8936d1
Danil Kurmayev <danilkurmayev@gmail.com>
2023-09-27 18:25:31 +0200

    updated

--- my-shop-main-page/src/components/card/Card.jsx
+++ my-shop-main-page/src/components/card/Card.jsx
@@ -1,22 +1,19 @@
 import styles from './Card.module.css'
-import cat01Img from './../../img/categories/cat-01.jpg'
 import arrowImg from './../../img/icons/arrow.svg'
-//import cat02Img from './../../img/categories/cat-02.jpg'
-//import cat03Img from './../../img/categories/cat-03.jpg'
 
 
 
-const Card = () => {
-   
+
+const Card = ({title, img}) => {  
     
     return <div className={styles.card}>
 
         <a href="#!" className={styles.card__link}>.</a>
 
-        <img className={styles.card__img} src={cat01Img} alt="cat01" />
+        <img className={styles.card__img} src={img} alt="cat01" />
         <div className={styles.card__body}>
             <div className={styles.card__text}>
-                <div className={styles.card__titel}>Hoodies & Sweetshirt</div>
+                <div className={styles.card__titel}>{title}</div>
                 <div className={styles.card__muted}>Explore Now!</div>
 
             </div>
