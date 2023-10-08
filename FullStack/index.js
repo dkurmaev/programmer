109dca6d592dcf160ff62ad5794b66fb319eec91
Danil Kurmayev <danilkurmayev@gmail.com>
2023-10-08 20:57:59 +0200

    updated

--- FullStack/index.js
+++ FullStack/index.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`Server is running ${port}`))
+app.listen(port, () => console.log(`Server is running on ${port}`))
