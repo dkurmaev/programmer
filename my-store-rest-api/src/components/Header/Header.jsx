3cb2731d8d55e7d0e43692fce558b2cf69531693
dkurmaev <danilkurmayev@gmail.com>
2023-11-17 18:30:51 +0100

    updated

--- my-store-rest-api/src/components/Header/Header.jsx
+++ my-store-rest-api/src/components/Header/Header.jsx
@@ -1,94 +1,122 @@
-import React, {useEffect, useState} from 'react'
-import {useDispatch, useSelector} from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import styles from '../../styles/Header.module.css'
-import {ROUTES} from "../../utils/routes";
-import LOGO from "../../images/logo.svg"
-import AVATAR from "../../images/avatar.jpg"
+import styles from "../../styles/Header.module.css";
 
-import {toggleForm} from "../../features/user/userSlice";
-import {useGetProductsQuery} from "../../features/api/apiSlice";
+import { ROUTES } from "../../utils/routes";
+
+import LOGO from "../../images/logo.svg";
+import AVATAR from "../../images/avatar.jpg";
+
+import { toggleForm } from "../../features/user/userSlice";
+import { useGetProductsQuery } from "../../features/api/apiSlice";
 
 const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const [searchValue, setSearchValue] = useState("");
-    const { currentUser } =useSelector(({ user }) => user)
-    const [values, setValues] = useState({
-        name: "Guest",
-        avatar: AVATAR,
-    })
-    const {data, isLoading} = useGetProductsQuery({ title: searchValue })
-    console.log(data)
+    const { currentUser, cart } = useSelector(({ user }) => user);
+
+    const [values, setValues] = useState({ name: "Guest", avatar: AVATAR });
+
+    const { data, isLoading } = useGetProductsQuery({ title: searchValue });
 
     useEffect(() => {
-        if(!currentUser) return;
-        setValues(currentUser)
-    }, [currentUser])
+        if (!currentUser) return;
+
+        setValues(currentUser);
+    }, [currentUser]);
 
     const handleClick = () => {
-        if(!currentUser) dispatch(toggleForm(true));
+        if (!currentUser) dispatch(toggleForm(true));
         else navigate(ROUTES.PROFILE);
-
     };
 
-    const handleSearch = ({target: { value }}) => {
-        setSearchValue(value)
-    }
-
+    const handleSearch = ({ target: { value } }) => {
+        setSearchValue(value);
+    };
 
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
                 <Link to={ROUTES.HOME}>
-                    <img src={LOGO} alt="Space Burger"/>
+                    <img src={LOGO} alt="Stuff" />
                 </Link>
             </div>
+
             <div className={styles.info}>
                 <div className={styles.user} onClick={handleClick}>
-                    <div className={styles.avatar} style={{backgroundImage: `url(${values.avatar})`}}
+                    <div
+                        className={styles.avatar}
+                        style={{ backgroundImage: `url(${values.avatar})` }}
                     />
                     <div className={styles.username}>{values.name}</div>
                 </div>
+
                 <form className={styles.form}>
                     <div className={styles.icon}>
                         <svg className="icon">
-                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}/>
+                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
                         </svg>
                     </div>
                     <div className={styles.input}>
                         <input
                             type="search"
                             name="search"
-                            placeholder="Search for anything..."
+                            placeholder="Search for anyting..."
                             autoComplete="off"
                             onChange={handleSearch}
                             value={searchValue}
                         />
                     </div>
-                    {<div className={styles.box}></div> && false}
-                    TODO: add search results
 
+                    {searchValue && (
+                        <div className={styles.box}>
+                            {isLoading
+                                ? "Loading"
+                                : !data.length
+                                    ? "No results"
+                                    : data.map(({ title, images, id }) => {
+                                        return (
+                                            <Link
+                                                key={id}
+                                                onClick={() => setSearchValue("")}
+                                                className={styles.item}
+                                                to={`/products/${id}`}
+                                            >
+                                                <div
+                                                    className={styles.image}
+                                                    style={{ backgroundImage: `url(${images[0]})` }}
+                                                />
+                                                <div className={styles.title}>{title}</div>
+                                            </Link>
+                                        );
+                                    })}
+                        </div>
+                    )}
                 </form>
+
                 <div className={styles.account}>
                     <Link to={ROUTES.HOME} className={styles.favourites}>
                         <svg className={styles["icon-fav"]}>
-                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`}/>
+                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
                         </svg>
                     </Link>
 
                     <Link to={ROUTES.CART} className={styles.cart}>
                         <svg className={styles["icon-cart"]}>
-                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`}/>
+                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
                         </svg>
-                        <span className={styles.count}>2</span>
+                        {!!cart.length && (
+                            <span className={styles.count}>{cart.length}</span>
+                        )}
                     </Link>
                 </div>
             </div>
         </div>
+    );
+};
 
-    )
-}
-export default Header
+export default Header;
--- my-store-rest-api/src/features/user/userSlice.js
+++ my-store-rest-api/src/features/user/userSlice.js
@@ -1,25 +1,32 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import {BASE_URL} from "../../utils/constants";
+import { BASE_URL } from "../../utils/constants";
 
+// Thunk для создания пользователя
 export const createUser = createAsyncThunk(
-    'users/getUser',
+    'users/createUser', // Уникальное имя для этого Thunk
     async (payload, thunkAPI) => {
         try {
+            // Отправляем POST-запрос для создания пользователя
             const res = await axios.post(`${BASE_URL}/users`, payload);
             return res.data;
         } catch (err) {
             console.log(err);
-            return thunkAPI.rejectWithValue({error: err.message});
+            // Если возникает ошибка, отклоняем Thunk с информацией об ошибке
+            return thunkAPI.rejectWithValue({ error: err.message });
         }
     }
 );
+
+// Thunk для входа пользователя
 export const loginUser = createAsyncThunk(
-    'users/loginUser',
+    'users/loginUser', // Уникальное имя для этого Thunk
     async (payload, thunkAPI) => {
         try {
+            // Отправляем POST-запрос для входа пользователя
             const res = await axios.post(`${BASE_URL}/auth/login`, payload);
-            const login = await axios(`${BASE_URL}/auth/profile`,{
+            // Получаем профиль пользователя после успешного входа
+            const login = await axios(`${BASE_URL}/auth/profile`, {
                 headers: {
                     "Authorization": `Bearer ${res.data.access_token}`
                 }
@@ -27,27 +34,34 @@ export const loginUser = createAsyncThunk(
             return login.data;
         } catch (err) {
             console.log(err);
-            return thunkAPI.rejectWithValue({error: err.message});
+            // Если возникает ошибка, отклоняем Thunk с информацией об ошибке
+            return thunkAPI.rejectWithValue({ error: err.message });
         }
     }
 );
+
+// Thunk для обновления данных пользователя
 export const updateUser = createAsyncThunk(
-    'users/updateUser',
+    'users/updateUser', // Уникальное имя для этого Thunk
     async (payload, thunkAPI) => {
         try {
+            // Отправляем PUT-запрос для обновления данных пользователя
             const res = await axios.put(`${BASE_URL}/users${payload.id}`, payload);
             return res.data;
         } catch (err) {
             console.log(err);
-            return thunkAPI.rejectWithValue({error: err.message});
+            // Если возникает ошибка, отклоняем Thunk с информацией об ошибке
+            return thunkAPI.rejectWithValue({ error: err.message });
         }
     }
 );
 
-const addCurrentUser = (state, {payload}) => {
+// Редюсер для обработки данных пользователя
+const addCurrentUser = (state, { payload }) => {
     state.currentUser = payload;
 }
 
+// Создаем срез состояния и редюсеры для пользовательского стора
 const userSlice = createSlice({
     name: 'user',
     initialState: {
@@ -59,43 +73,53 @@ const userSlice = createSlice({
         showForm: false,
     },
     reducers: {
-        addItemToCart: (state, {payload}) => {
+        // Редюсер для добавления товара в корзину
+        addItemToCart: (state, { payload }) => {
             let newCart = [...state.cart];
-            const found = state.cart.find(({id}) => id === payload.id);
+            const found = state.cart.find(({ id }) => id === payload.id);
             if (found) {
                 newCart = newCart.map((item) => {
                     return item.id === payload.id
-                        ? {...item, quantity: payload.quantity || item.quantity + 1}
+                        ? { ...item, quantity: payload.quantity || item.quantity + 1 }
                         : item;
                 });
             } else {
-                newCart.push({...payload, quantity: 1});
+                newCart.push({ ...payload, quantity: 1 });
             }
             state.cart = newCart;
         },
 
-
-        addItemToFavorites: (state, {payload}) => {
-            const found = state.favorites.find(({id}) => id === payload.id);
+        // Редюсер для добавления товара в избранное
+        addItemToFavorites: (state, { payload }) => {
+            const found = state.favorites.find(({ id }) => id === payload.id);
             if (!found) {
-                state.favorites.push({...payload, isFavorite: true});
+                state.favorites.push({ ...payload, isFavorite: true });
             }
         },
 
-        toggleForm: (state, {payload}) => {
+        // Редюсер для переключения видимости формы
+        toggleForm: (state, { payload }) => {
             state.showForm = payload;
         },
-        toggleFormType:  (state, {payload}) => {
+
+        // Редюсер для переключения типа формы (signup/login)
+        toggleFormType: (state, { payload }) => {
             state.formType = payload;
         },
-    },
 
+        // Редюсер для удаления товара из корзины
+        removeItemFromCart: (state, { payload }) => {
+            state.cart = state.cart.filter(({ id }) => id !== payload);
+        },
+    },
     extraReducers: (builder) => {
+        // Добавляем обработку успешного завершения Thunk'ов для обновления данных пользователя
         builder.addCase(createUser.fulfilled, addCurrentUser);
-        builder.addCase(loginUser.fulfilled,addCurrentUser);
-        builder.addCase(updateUser.fulfilled,addCurrentUser);
+        builder.addCase(loginUser.fulfilled, addCurrentUser);
+        builder.addCase(updateUser.fulfilled, addCurrentUser);
     },
 });
 
-export const {addItemToCart, addItemToFavorites, toggleForm, toggleFormType} = userSlice.actions;
+// Экспортируем созданные редюсеры и действия
+export const { addItemToCart, addItemToFavorites, toggleForm, toggleFormType, removeItemFromCart } = userSlice.actions;
 export default userSlice.reducer;
